8960a19603221a2d51fabe768d7995158201ee78
Stephen Lorence <sklorence@gmail.com>
2021-09-28 20:22:46 -0500

    First commit, added REST API to access items, stores, and user authentication.

---
+++ models/item.py
@@ -0,0 +1,40 @@
+from db import db
+
+class ItemModel(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    price = db.Column(db.Float(precision=2))
+
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    store = db.relationship('StoreModel', backref='items')
+
+    def __init__(self, name, price, store_id):
+        self.name = name
+        self.price = price
+        self.store_id = store_id
+
+    def json(self):
+        return {"name": self.name, "price": self.price, "store_id": self.store_id}
+
+    @classmethod
+    def find_by_name(cls, name):
+        try:
+            return cls.query.filter_by(name=name).first()
+        except:
+            return {"message": "An error occurred finding the item."}, 500
+
+    def save_to_db(self):
+        try:
+           db.session.add(self)
+           db.session.commit()
+        except:
+            return {"message": "An error occurred inserting the item."}, 500
+
+    def delete_from_db(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+        except:
+            return {"message": "An error occurred deleting the item."}, 500
---
+++ models/store.py
@@ -0,0 +1,40 @@
+from sqlalchemy.orm import backref
+from db import db
+
+class StoreModel(db.Model):
+    __tablename__ = 'stores'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+    # items = db.relationship('ItemModel', backref='store')
+
+    def __init__(self, name):
+        self.name = name
+
+    def json(self):
+        return {"name": self.name, "id": self.id, "items": [{
+                                                                "name": item.name,
+                                                                "price": item.price
+                                                            } for item in self.items]}
+
+    @classmethod
+    def find_by_name(cls, name):
+        try:
+            return cls.query.filter_by(name=name).first()
+        except:
+            return {"message": "An error occurred finding the store."}, 500
+
+    def save_to_db(self):
+        try:
+           db.session.add(self)
+           db.session.commit()
+        except:
+            return {"message": "An error occurred inserting the store."}, 500
+
+    def delete_from_db(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+        except:
+            return {"message": "An error occurred deleting the store."}, 500
